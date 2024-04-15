b90b4ee844fda623833a2131f1359b3718121376
Guillermo Roldán Sienra <groldan@domingoalonsogroup.com>
2024-04-15 10:40:52 +0100

    Añadida logica y test para cuando la máquina deja de ser gratis

--- coffee-machine-b/CoffeeMachine.Tests/core/CoffeeMachineTest.cs
+++ coffee-machine-b/CoffeeMachine.Tests/core/CoffeeMachineTest.cs
@@ -11,6 +11,7 @@ namespace CoffeeMachine.Tests.core;
 public class CoffeeMachineTest
 {
     private const string SelectDrinkMessage = "Please, select a drink!";
+    private string NotEnoughMoneyMessage = "Please, Remains to introduce #!";
     private CoffeeMachine.core.CoffeeMachine _coffeeMachine;
     private DrinkMakerDriver _drinkMakerDriver;
 
@@ -18,12 +19,13 @@ public class CoffeeMachineTest
     public void SetUp()
     {
         _drinkMakerDriver = Substitute.For<DrinkMakerDriver>();
-        _coffeeMachine = new CoffeeMachine.core.CoffeeMachine(_drinkMakerDriver);
+        FreeCoffeeMachine();
     }
-
+    
     [Test]
     public void Make_Chocolate()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         _coffeeMachine.SelectChocolate();
         _coffeeMachine.MakeDrink();
 
@@ -33,6 +35,7 @@ public class CoffeeMachineTest
     [Test]
     public void Make_Tea()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         _coffeeMachine.SelectTea();
         _coffeeMachine.MakeDrink();
 
@@ -42,6 +45,7 @@ public class CoffeeMachineTest
     [Test]
     public void Make_Coffee()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         _coffeeMachine.SelectCoffee();
         _coffeeMachine.MakeDrink();
 
@@ -51,6 +55,7 @@ public class CoffeeMachineTest
     [Test]
     public void Make_Any_Drink_With_1_Spoon_Of_Sugar()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         _coffeeMachine.SelectChocolate();
         _coffeeMachine.AddOneSpoonOfSugar();
         _coffeeMachine.MakeDrink();
@@ -61,6 +66,7 @@ public class CoffeeMachineTest
     [Test]
     public void Make_Any_Drink_With_2_Spoons_Of_Sugar()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         _coffeeMachine.SelectChocolate();
         _coffeeMachine.AddOneSpoonOfSugar();
         _coffeeMachine.AddOneSpoonOfSugar();
@@ -72,6 +78,7 @@ public class CoffeeMachineTest
     [Test]
     public void Make_Any_Drink_With_More_Than_2_Spoons_Of_Sugar()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         _coffeeMachine.SelectChocolate();
         _coffeeMachine.AddOneSpoonOfSugar();
         _coffeeMachine.AddOneSpoonOfSugar();
@@ -84,6 +91,7 @@ public class CoffeeMachineTest
     [Test]
     public void Warns_The_User_When_No_Drink_Was_Selected()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         _coffeeMachine.MakeDrink();
 
         _drinkMakerDriver.Received().Notify(Message.Create(SelectDrinkMessage));
@@ -92,6 +100,7 @@ public class CoffeeMachineTest
     [Test]
     public void Resets_Drink_After_Making_Drink()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         AfterMakingDrink();
 
         _coffeeMachine.MakeDrink();
@@ -103,6 +112,7 @@ public class CoffeeMachineTest
     [Test]
     public void Resets_Sugar_After_Making_Drink()
     {
+        var _coffeeMachine = FreeCoffeeMachine();
         var ordersSent = CaptureSentOrders();
         AfterMakingDrinkWithSugar();
 
@@ -110,6 +120,39 @@ public class CoffeeMachineTest
         _coffeeMachine.MakeDrink();
 
         Assert.That(ordersSent.Last(), Is.EqualTo(Coffee().Build()));
+    }
+
+    [Test]
+    public void Not_Make_Coffee_because_not_enough_money()
+    {
+        var _coffeeMachine = NotFreeCoffeeMachine();
+        _coffeeMachine.SelectCoffee();
+        _coffeeMachine.AddMoney(0.2m);
+        _coffeeMachine.MakeDrink();
+
+        _drinkMakerDriver.Received().Notify(Message.Create(NotEnoughMoneyMessage.Replace("#","0,4")));
+    }
+
+    [Test]
+    public void Not_Make_Chocolate_because_not_enough_money()
+    {
+        var _coffeeMachine = NotFreeCoffeeMachine();
+        _coffeeMachine.SelectChocolate();
+        _coffeeMachine.AddMoney(0.2m);
+        _coffeeMachine.MakeDrink();
+
+        _drinkMakerDriver.Received().Notify(Message.Create(NotEnoughMoneyMessage.Replace("#", "0,3")));
+    }
+
+    [Test]
+    public void Not_Make_Tea_because_not_enough_money()
+    {
+        var _coffeeMachine = NotFreeCoffeeMachine();
+        _coffeeMachine.SelectTea();
+        _coffeeMachine.AddMoney(0.2m);
+        _coffeeMachine.MakeDrink();
+
+        _drinkMakerDriver.Received().Notify(Message.Create(NotEnoughMoneyMessage.Replace("#", "0,2")));
     }
 
     private List<Order> CaptureSentOrders()
@@ -130,5 +173,16 @@ public class CoffeeMachineTest
         _coffeeMachine.SelectTea();
         _coffeeMachine.AddOneSpoonOfSugar();
         _coffeeMachine.MakeDrink();
+    }
+    private CoffeeMachine.core.CoffeeMachine FreeCoffeeMachine()
+    {
+        _coffeeMachine = new CoffeeMachine.core.CoffeeMachine(_drinkMakerDriver);
+        _coffeeMachine.AddMoney(10);
+        return _coffeeMachine;
+    }
+    private CoffeeMachine.core.CoffeeMachine NotFreeCoffeeMachine()
+    {
+        _coffeeMachine = new CoffeeMachine.core.CoffeeMachine(_drinkMakerDriver);
+        return _coffeeMachine;
     }
 }
--- coffee-machine-b/CoffeeMachine/core/CoffeeMachine.cs
+++ coffee-machine-b/CoffeeMachine/core/CoffeeMachine.cs
@@ -1,14 +1,26 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
 namespace CoffeeMachine.core;
 
 public class CoffeeMachine
 {
     private readonly DrinkMakerDriver _drinkMakerDriver;
     private Order _order;
+    private decimal _amount;
+    private Dictionary<DrinkType, decimal> _drinkTypesPrices;
 
     public CoffeeMachine(DrinkMakerDriver drinkMakerDriver)
     {
         _drinkMakerDriver = drinkMakerDriver;
         _order = new Order();
+        _drinkTypesPrices = new Dictionary<DrinkType, decimal>()
+        {
+            { DrinkType.Chocolate ,0.5m},
+            { DrinkType.Tea ,0.4m},
+            { DrinkType.Coffee ,0.6m},
+            { DrinkType.None ,0m}
+        };
     }
 
     public void SelectChocolate()
@@ -39,8 +51,26 @@ public class CoffeeMachine
             return;
         }
 
+        if (_amount < GetDrinkTypesPrice())
+        {
+            DisplayNotEnoughMoney();
+            return;
+        }
+        
+
         _drinkMakerDriver.Send(_order);
         _order = new Order();
+    }
+
+    private void DisplayNotEnoughMoney()
+    {
+        var difference = GetDrinkTypesPrice() - _amount;
+        _drinkMakerDriver.Notify(NotEnoughMoneyMessage(difference));
+    }
+
+    private decimal GetDrinkTypesPrice()
+    {
+        return _drinkTypesPrices[_order.GetDrinkType()];
     }
 
     private bool NoDrinkWasSelected()
@@ -53,4 +83,16 @@ public class CoffeeMachine
         const string message = "Please, select a drink!";
         return Message.Create(message);
     }
+
+    private Message NotEnoughMoneyMessage(decimal difference)
+    {
+        var message = $"Please, Remains to introduce {difference}!";
+        return Message.Create(message);
+    }
+
+    public void AddMoney(decimal amount)
+    {
+        _amount = amount;
+    }
+
 }
