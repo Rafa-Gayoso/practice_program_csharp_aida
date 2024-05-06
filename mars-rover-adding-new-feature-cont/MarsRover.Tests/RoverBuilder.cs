d528966696c395605b452e81efb9f0d48562afe2
Rafa Gayoso <rgayoso@aidacanarias.com>
2024-05-06 16:19:08 +0200

    ♻️Rover Builder to Setup

---
+++ mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverBuilder.cs
@@ -0,0 +1,37 @@
+namespace MarsRover.Tests;
+
+public class RoverBuilder
+{
+    private string direction = string.Empty;
+    private int x = 0;
+    private int y = 0;
+
+    public static RoverBuilder ARover()
+    {
+        return new RoverBuilder();
+    }
+
+    public RoverBuilder Facing(string direction)
+    {
+        this.direction = direction;
+        return this;
+    }
+
+    public RoverBuilder WithCoordinates(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    public Rover Build()
+    {
+        return new Rover(x, y, direction);
+    }
+
+    public static Rover RoverFacing(string direction)
+    {
+        return ARover().Facing(direction).Build();
+    }
+
+}
--- mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverEqualityTest.cs
+++ mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverEqualityTest.cs
@@ -5,63 +5,33 @@ namespace MarsRover.Tests;
 
 public class RoverEqualityTest
 {
+    private RoverBuilder _roverFacingNorth;
+
+    [SetUp]
+    public void Initialize()
+    {
+        _roverFacingNorth = ARover().Facing("N").WithCoordinates(1, 1);
+    }
+
     [Test]
     public void Equal_Rovers()
     {
-        Assert.That(
-            ARover().Facing("N").WithCoordinates(1, 1).Build(),
-            Is.EqualTo(ARover().Facing("N").WithCoordinates(1, 1).Build()));
+        Assert.That(_roverFacingNorth.Build(), Is.EqualTo(_roverFacingNorth.Build()));
     }
 
     [Test]
     public void Not_Equal_Rovers()
     {
         Assert.That(
-            ARover().Facing("N").WithCoordinates(1, 1).Build(),
+            _roverFacingNorth.Build(),
             Is.Not.EqualTo(ARover().Facing("S").Build()));
 
         Assert.That(
-            ARover().Facing("N").WithCoordinates(1, 1).Build(),
+            _roverFacingNorth.Build(),
             Is.Not.EqualTo(ARover().Facing("N").WithCoordinates(1, 2).Build()));
 
         Assert.That(
-            ARover().Facing("N").WithCoordinates(1, 1).Build(),
+            _roverFacingNorth.Build(),
             Is.Not.EqualTo(ARover().Facing("N").WithCoordinates(0, 1).Build()));
     }
-}
-
-internal class RoverBuilder
-{
-    private string direction = string.Empty;
-    private int x = 0;
-    private int y = 0;
-
-    public static RoverBuilder ARover()
-    {
-        return new RoverBuilder();
-    }
-
-    public RoverBuilder Facing(string direction)
-    {
-        this.direction = direction;
-        return this;
-    }
-
-    public RoverBuilder WithCoordinates(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
-        return this;
-    }
-
-    public Rover Build()
-    {
-        return new Rover(x, y, direction);
-    }
-
-    public static Rover RoverFacing(string direction)
-    {
-        return ARover().Facing(direction).Build();
-    }
-
 }
