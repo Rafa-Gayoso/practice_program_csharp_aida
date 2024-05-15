cae5825f76253cbc6b61bc08017c4faef4465480
Ramón Martínez <ramartinez@aidacanarias.com>
2024-05-15 13:40:23 +0200

    add_not_available_product

--- shopping-cart/ShoppingCart.Tests/ShoppingCartTest.cs
+++ shopping-cart/ShoppingCart.Tests/ShoppingCartTest.cs
@@ -1,13 +1,61 @@
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using NUnit.Framework;
 
-namespace ShoppingCart.Tests
+namespace ShoppingCart.Tests;
+
+public class ShoppingCartTest
 {
-    public class ShoppingCartTest
+    private ProductsRepository _productsRepository;
+    private Notifier _notifier;
+
+    [SetUp]
+    public void SetUp()
     {
-        [Test]
-        public void Canary_Test()
-        {
-            Assert.That(true, Is.True);
-        }
+        _productsRepository = Substitute.For<ProductsRepository>();
+        _notifier = Substitute.For<Notifier>();
     }
+
+    [Test]
+    public void add_not_available_product()
+    {
+        ShoppingCart shoppingCart = new(_productsRepository, _notifier);
+        _productsRepository.Get("some_item").ReturnsNull();
+
+        shoppingCart.AddItem("some_item");
+
+        _notifier.Received(1).ShowError("Product is not available");
+    }
+}
+
+public class ShoppingCart
+{
+    private readonly ProductsRepository _productsRepository;
+    private readonly Notifier _notifier;
+
+    public ShoppingCart(ProductsRepository productsRepository, Notifier notifier)
+    {
+        _productsRepository = productsRepository;
+        _notifier = notifier;
+    }
+
+    public void AddItem(string productName)
+    {
+        _notifier.ShowError("Product is not available");
+    }
+}
+
+public interface Notifier
+{
+    void ShowError(string message);
+}
+
+public interface ProductsRepository
+{
+    Product Get(string productName);
+}
+
+public class Product
+{
+
 }
