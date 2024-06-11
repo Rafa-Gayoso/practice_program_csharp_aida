0c4dc903f5e62122bccd463c25ab514764921db8
Rafa Gayoso <rgayoso@aidacanarias.com>
2024-06-11 10:31:14 +0200

    ✅greet_with_good_morning

--- hello-service/HelloService.Tests/HelloServiceTest.cs
+++ hello-service/HelloService.Tests/HelloServiceTest.cs
@@ -5,15 +5,34 @@ namespace Hello.Tests
 {
     public class HelloServiceTest
     {
+        private Notifier _notifier;
+        private TimeProvider _dateProvider;
+        private HelloService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _notifier = Substitute.For<Notifier>();
+            _dateProvider = Substitute.For<TimeProvider>();
+            _service = new HelloService(_notifier, _dateProvider);
+        }
+
         [Test]
         public void greet_with_good_night()
         {
-            var notifier = Substitute.For<Notifier>();
-            var service = new HelloService(notifier);
+            _service.Hello();
 
-            service.Hello();
+            _notifier.Received(1).Notify("Buenas noches!");
+        }
 
-            notifier.Received(1).Notify("Buenas noches!");
+        [Test]
+        public void greet_with_good_morning()
+        {
+            _dateProvider.GetTime().Returns(new TimeOnly(8, 0, 0));
+
+            _service.Hello();
+
+            _notifier.Received(1).Notify("Buenos dias!");
         }
     }
 }
--- hello-service/HelloService/HelloService.cs
+++ hello-service/HelloService/HelloService.cs
@@ -1,16 +1,30 @@
+using System;
+
 namespace Hello;
 
 public class HelloService
 {
     private readonly Notifier _notifier;
+    private readonly TimeProvider _timeProvider;
 
     public HelloService(Notifier notifier)
     {
         _notifier = notifier;
     }
 
+    public HelloService(Notifier notifier, TimeProvider timeProvider)
+    {
+        _notifier = notifier;
+        _timeProvider = timeProvider;
+    }
+
     public void Hello()
     {
+        var time = _timeProvider.GetTime();
+        if (time.IsBetween(new TimeOnly(6,0,0),new TimeOnly(12,0,0)))
+        {
+            _notifier.Notify("Buenos dias!");
+        }
         _notifier.Notify("Buenas noches!");
     }
 }
