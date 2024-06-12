5be065d9057955f5c35596414f5bed59ac1539fb
Rafa Gayoso <rgayoso@aidacanarias.com>
2024-06-12 13:10:40 +0200

    ♻️refector OrderSummaryPresenterTest

--- stock-broker-service/StockBroker.Tests/OrderSummaryPresenterTest.cs
+++ stock-broker-service/StockBroker.Tests/OrderSummaryPresenterTest.cs
@@ -5,40 +5,40 @@ namespace StockBroker.Tests
 {
     public class OrderSummaryPresenterTest
     {
+        private Notifier _notifier;
+        private OrderSummaryPresenter _presenter;
+        private DateOnly _orderDate;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _notifier = Substitute.For<Notifier>();
+            _orderDate = new DateOnly(2024, 6, 12);
+            _presenter = new OrderSummaryPresenter(_notifier);
+        }
         
         [Test]
         public void print_no_orders_summary()
         {
-            var notifier = Substitute.For<Notifier>();
-            Presenter presenter = new OrderSummaryPresenter(notifier);
-            var orderDate = new DateOnly(2024, 6, 12);
+            _presenter.Present(new OrderSummary(_orderDate, 0, 0));
 
-            presenter.Present(new OrderSummary(orderDate, 0, 0));
-
-            notifier.Received(1).Notify($"{orderDate} Buy: € 0.00, Sell: € 0.00");
+            _notifier.Received(1).Notify($"{_orderDate} Buy: € 0.00, Sell: € 0.00");
         }
 
         [Test]
         public void print_orders_summary_with_stock_bought()
         {
-            var notifier = Substitute.For<Notifier>();
-            Presenter presenter = new OrderSummaryPresenter(notifier);
-            var orderDate = new DateOnly(2024, 6, 12);
+            _presenter.Present(new OrderSummary(_orderDate, 59.6m, 0m));
 
-            presenter.Present(new OrderSummary(orderDate, 59.6m, 0m));
-
-            notifier.Received(1).Notify($"{orderDate} Buy: € 59.60, Sell: € 0.00");
+            _notifier.Received(1).Notify($"{_orderDate} Buy: € 59.60, Sell: € 0.00");
         }
+
         [Test]
         public void print_orders_summary_with_stock_sold()
         {
-            var notifier = Substitute.For<Notifier>();
-            Presenter presenter = new OrderSummaryPresenter(notifier);
-            var orderDate = new DateOnly(2024, 6, 12);
+            _presenter.Present(new OrderSummary(_orderDate, 0m, 12.5m));
 
-            presenter.Present(new OrderSummary(orderDate, 0m, 12.5m));
-
-            notifier.Received(1).Notify($"{orderDate} Buy: € 0.00, Sell: € 12.50");
+            _notifier.Received(1).Notify($"{_orderDate} Buy: € 0.00, Sell: € 12.50");
         }
     }
 }
